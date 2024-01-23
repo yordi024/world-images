4e6e7c4cab784770c37a5f3c0a2d462e8851d92c
Yordi Cartagena <yordicartagena24@gmail.com>
2024-01-23 14:47:14 -0400

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'vue/multi-word-component-names': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main><h1>Hello World</h1></main>
+</template>
