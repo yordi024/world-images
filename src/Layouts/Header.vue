9133df828a72588ef84d14a531c491b6cd70788b
Yordi Cartagena <yordicartagena24@gmail.com>
2024-01-23 19:48:32 -0400

    Add dark/light theme toggle

--- src/Layouts/Header.vue
+++ src/Layouts/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border-b">
     <div class="flex h-16 items-center px-4">
-      <RouterLink to="/" class="mr-5 font-bold text-lg">WI</RouterLink>
+      <RouterLink to="/" class="mr-5 font-bold text-lg text-primary"><Globe2 /></RouterLink>
       <nav class="flex items-center space-x-4 lg:space-x-6">
         <RouterLink
           v-for="item in menu"
@@ -13,6 +13,10 @@
         </RouterLink>
       </nav>
       <div class="ml-auto flex items-center space-x-4">
+        <Button @click="toggleDark()" class="px-2" size="sm" variant="ghost">
+          <Sun v-if="isDark" class="h-5" />
+          <Moon v-else class="h-5" />
+        </Button>
         <div class="text-sm font-medium transition-colors text-primary">Admin</div>
       </div>
     </div>
@@ -20,11 +24,14 @@
 </template>
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { Button } from '@/components/ui/button'
+import { useDark, useToggle } from '@vueuse/core/index.cjs'
+import { Globe2, Moon, Sun } from 'lucide-vue-next'
 
 const menu = [
   {
     name: 'Home',
-    url: '/',
+    url: '/home',
     route: 'home',
   },
   {
@@ -33,4 +40,12 @@ const menu = [
     route: 'sellers',
   },
 ]
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
+<style>
+a.router-link-active {
+  @apply text-primary;
+}
+</style>
