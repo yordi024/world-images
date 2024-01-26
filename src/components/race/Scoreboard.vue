ca03259785943d4aae9ce4701ea3622a33cd503a
Yordi Cartagena <yordicartagena24@gmail.com>
2024-01-26 19:26:20 -0400

    Add scoreboard modal initial design

---
+++ src/components/race/Scoreboard.vue
@@ -0,0 +1,103 @@
+<template>
+  <Dialog>
+    <DialogTrigger as-child> <Button variant="primary">Scoreboard</Button></DialogTrigger>
+    <DialogContent class="">
+      <DialogHeader>
+        <DialogTitle>Race Scoreboard</DialogTitle>
+        <DialogDescription> A seller needs 20 points to win the race. </DialogDescription>
+      </DialogHeader>
+      <ul class="block divide-y-4 divide-gray-200 h-22 overflow-hidden">
+        <li v-for="(score, key) in scoreBoard" :key="key" class="flex items-center my-2">
+          <span class="text-xl font-bold mr-4">{{ key + 1 }}.</span>
+          <div class="flex-1 w-full">
+            <h3 class="text-lg font-medium">{{ score.seller.name }}</h3>
+            <!-- <p class="text-primary">
+              <span class="font-semibold">{{ score.points }}</span
+              >/20
+            </p> -->
+          </div>
+          <Progress class="w-20" :modelValue="(score.points * 100) / 20" />
+        </li>
+      </ul>
+    </DialogContent>
+  </Dialog>
+</template>
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Button } from '@/components/ui/button'
+import { Progress } from '@/components/ui/progress'
+import { ref } from 'vue'
+
+const scoreBoard = ref([
+  {
+    seller: {
+      id: 1,
+      name: 'Juan Perez',
+    },
+    points: 15,
+  },
+  {
+    seller: {
+      id: 1,
+      name: 'Juan Perez',
+    },
+    points: 10,
+  },
+  {
+    seller: {
+      id: 1,
+      name: 'Juan Perez',
+    },
+    points: 10,
+  },
+  {
+    seller: {
+      id: 1,
+      name: 'Juan Perez',
+    },
+    points: 10,
+  },
+  {
+    seller: {
+      id: 1,
+      name: 'Juan Perez',
+    },
+    points: 10,
+  },
+  {
+    seller: {
+      id: 1,
+      name: 'Juan Perez',
+    },
+    points: 10,
+  },
+  {
+    seller: {
+      id: 1,
+      name: 'Juan Perez',
+    },
+    points: 10,
+  },
+  {
+    seller: {
+      id: 1,
+      name: 'Juan Perez',
+    },
+    points: 10,
+  },
+  {
+    seller: {
+      id: 1,
+      name: 'Juan Perez',
+    },
+    points: 10,
+  },
+])
+</script>
---
+++ src/components/race/WinnerBanner.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="w-full flex border-primary/50 text-primary dark:border-primary items-center justify-between max-w-3xl rounded-lg p-4 border"
+  >
+    <Medal class="mr-2" />
+    <div class="text-2xl font-bold flex-1">Juan Perez</div>
+    <Button>Scoreboard</Button>
+  </div>
+</template>
+<script setup lang="ts">
+import { Medal } from 'lucide-vue-next'
+</script>
